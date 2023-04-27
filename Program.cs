084a40d98749b4aed4a295992c405571fcb9efc0
ngimbisarah243 <116262116+ngimbisarah243@users.noreply.github.com>
2023-04-27 12:38:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿var rand = new Random(4711);
+double minValue = 1_000_000_001;
+double maxValue = 0;
+
+for (int i = 0; i < 1_000_000; i++)
+{
+     int randomNumber = rand.Next(1,1_000_000_001);
+
+    minValue = randomNumber < minValue ? randomNumber : minValue;
+    maxValue = randomNumber > maxValue ? randomNumber : maxValue;
+}
+
+Console.WriteLine((maxValue-minValue)/999_999);
+
